5259592fc40dc133d2d521355f0b27181d35dd66
Moisés Lopes Nóbrega <20241101110019@ifrn.local>
2025-11-26 16:07:04 -0300

    feat: divisão de arquivo das classes, classe Game( )

---
+++ game/main.py
@@ -0,0 +1,78 @@
+from classes import Jogador, Tiro, RoboZigueZague
+import pygame
+import random
+
+pygame.init()
+
+LARGURA = 600 
+ALTURA = 720
+FPS = 60
+
+class Game():
+    def __init__(self, largura, altura, FPS):
+        self.largura = largura
+        self.altura = altura
+        self.clock = pygame.time.Clock()
+        self.FPS = FPS
+        self.todos_sprites = pygame.sprite.Group()
+        self.inimigos = pygame.sprite.Group()
+        self.tiros = pygame.sprite.Group()
+        self.jogador = Jogador(LARGURA // 2, ALTURA - 60)
+        self.todos_sprites.add(self.jogador)
+        self.pontos = 0
+        self.spawn_timer = 0
+    
+    def rodar(self):
+        TELA = pygame.display.set_mode((LARGURA, ALTURA))
+        pygame.display.set_caption("Robot Defense - Template")
+        rodando = True
+        while rodando:
+            self.clock.tick(self.FPS)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    rodando = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        tiro = Tiro(self.jogador.rect.centerx, self.jogador.rect.y)
+                        self.todos_sprites.add(tiro)
+                        self.tiros.add(tiro)
+
+            # timer de entrada dos inimigos
+            self.spawn_timer += 1
+            if self.spawn_timer > 40:
+                robo = RoboZigueZague(random.randint(40, LARGURA - 40), -40)
+                self.todos_sprites.add(robo)
+                self.inimigos.add(robo)
+                self.spawn_timer = 0
+
+            # colisão tiro x robô
+            colisao = pygame.sprite.groupcollide(self.inimigos, self.tiros, True, True)
+            self.pontos += len(colisao)
+
+            # colisão robô x jogador
+            if pygame.sprite.spritecollide(self.jogador, self.inimigos, True):
+                self.jogador.vida -= 1
+                if self.jogador.vida <= 0:
+                    print("GAME OVER!")
+                    rodando = False
+
+            # atualizar
+            self.todos_sprites.update()
+
+            # desenhar
+            TELA.fill((20, 20, 20))
+            self.todos_sprites.draw(TELA)
+
+            #Painel de pontos e vida
+            font = pygame.font.SysFont(None, 30)
+            texto = font.render(f"Vida: {self.jogador.vida}  |  Pontos: {self.pontos}", True, (255, 255, 255))
+            TELA.blit(texto, (10, 10))
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+jogo = Game(LARGURA, ALTURA, FPS)
+jogo.rodar()
